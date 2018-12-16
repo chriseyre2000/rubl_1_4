b5489f198c1a76cc3f67c0a56c8754a14e9c345e
Christopher Eyre <chriseyre2000@gmail.com>
2018-12-16 18:12:55 +0000

    Added database migration, first table and users

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -3,27 +3,24 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context
   """
-
   alias Rumbl.Accounts.User
+  alias Rumbl.Repo
 
-  def list_users do
-    [
-      %User{id: "1", name: "Jose", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
-  end
 
   def get_user(id) do
-    list_users()
-    |> Enum.find(fn map -> map.id == id end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    list_users()
-    |> Enum.find(fn map -> 
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
   end
   
 end
